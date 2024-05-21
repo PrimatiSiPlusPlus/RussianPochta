e06ac3db0a20a96578aac79b4c7cfca64cd61908
soffochka100 <148487087+soffochka100@users.noreply.github.com>
2024-05-21 19:04:32 +0300

    write function and postoffice

--- RussianPochta/RussianPochta/function.cpp
+++ RussianPochta/RussianPochta/function.cpp
@@ -141,3 +141,96 @@ void PostOfficeControl::print() {
         cout << i + 1 << ". " << "\n" << parcel[i];
     }
 }
+
+void PostOfficeControl::createPostOffice(const int n) {
+    vector<PostOffice> arrayPostOffice = readPostOffice();
+    int countPost = getCountPostOffice();
+  vector<PostOffice> arrayPostOfficeNew(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arrayPostOfficeNew[i];
+    }
+
+    // Niaiauaao aaa aaoi?a a iaei ,noa?ue ec oaeea e iiaue nicaaiiue
+    arrayPostOffice.insert(arrayPostOffice.end(), arrayPostOfficeNew.begin(), arrayPostOfficeNew.end());
+    savePostOffice(arrayPostOffice,n + countPost);// nio?aiyai ana a oaee
+}
+
+void PostOfficeControl::removePostOffice() {
+    int deleteOffice;
+    vector<PostOffice> postOffice = readPostOffice();
+    for (int i = 0; i < getCountPostOffice(); i++) {
+        cout << i+1 <<". " << postOffice[i];
+    }
+    cout << "Eaeia ioaaeaiea ii?ou au oioeoa oaaeeou? Aaaaeoa iiia? ioaaeaiey: " << endl;
+    deleteOffice = inputInteger();
+    postOffice.erase(postOffice.begin() + deleteOffice - 1);
+    savePostOffice(postOffice, getCountPostOffice() - 1);
+}
+
+void PostOfficeControl::editPostOffice() {
+    int numberForEdit;
+    string newNamePostOffice;
+    vector<PostOffice> office = readPostOffice();
+    for (int i = 0; i < getCountPostOffice(); i++) {
+        cout << i+1<< ". " << office[i];
+    }
+    cout << "Eiy o eaeiai ioaaeaiey ii?ou au oioeoa eciaieou? Aaaaeoa iiia? ioaaeaiey: ";
+    while (true) {
+        numberForEdit = inputInteger();
+        if (numberForEdit > 0 && numberForEdit <= getCountPostOffice())
+            break;
+    }
+    cout << "Aaaaeoa iiaia eiy:";
+    cin >> newNamePostOffice;
+    office[numberForEdit - 1].setNameOffice(newNamePostOffice);
+    savePostOffice(office, getCountPostOffice());
+}
+
+void PostOfficeControl::savePostOffice(vector<PostOffice> arr, const int& n) {
+    ofstream file("filePostOffice.txt");
+    if (!file.is_open()) {
+        cout << "Error: could not open file.txt for saving" << endl;
+        return;
+    }
+    file << n << endl;
+    for (int i = 0; i < n; ++i) {
+        file << arr[i].getIndex() << " " << arr[i].getNameOffice() << " " << arr[i].getCoordinateX() << " " << arr[i].getCoordinateY() << std::endl;
+    }
+    file.close();
+};
+
+vector<PostOffice> PostOfficeControl::readPostOffice() {
+    int countPostOffice;
+    int index;
+    string name;
+    int x;
+    int y;
+    std::ifstream file("filePostOffice.txt");
+    if (!file.is_open()) {
+        std::cout << "Error: could not open file.txt for loading" << std::endl;
+        return {};
+    }
+    file >> countPostOffice;
+    vector<PostOffice> arrayPostOffice(countPostOffice);
+    for (int i = 0; i < countPostOffice; ++i) {
+        file >> index >> name >> x >> y;
+        arrayPostOffice[i].setIndex(index);
+        arrayPostOffice[i].setNameOffice(name);
+        arrayPostOffice[i].setCoordinateX(x);
+        arrayPostOffice[i].setCoordinateY(y);
+    }
+    file.close();
+    return arrayPostOffice;
+}
+
+int PostOfficeControl::getCountPostOffice() {
+    int n = 0;
+    std::ifstream file("filePostOffice.txt");
+    if (!file.is_open()) {
+        std::cout << "Error: could not open file.txt for loading" << std::endl;
+        return n;
+    }
+    file >> n;
+    file.close();
+    return n;
+}
